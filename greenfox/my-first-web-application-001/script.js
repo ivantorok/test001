8b9083d56fd98e6bace7d23876dfc7c9ca257e6f
ivan.torok.e <ivan.torok.at.emarsys@gmail.com>
2020-05-05 20:47:54 +0200

    images html css
    collected image files
    started dividing the html
    and started applying the firs css rules
    using id-s mainly....

---
+++ greenfox/my-first-web-application-001/script.js
@@ -0,0 +1,27 @@
+$('#left').click(function(e) {
+    e.preventDefault();
+
+});
+
+$('#right').click(function(e) {
+    e.preventDefault();
+
+});
+
+$('#thumbnails').click(function(e) {
+    e.preventDefault();
+
+});
+
+function image(title, text, filename) {
+    this.title = '',
+        this.text = '',
+        this.filename = '',
+}
+
+let images
+
+
+let loadimage = (image) => {
+
+}
